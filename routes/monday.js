20b5d7248ef6c55115399dfd44b29c14ac5c2677
Marcus Gainey <marcusgainey@Marcuss-MacBook-Air.local>
2022-07-05 23:32:42 -0400

    updates to monday interface

--- routes/monday.js
+++ routes/monday.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const mondayRouter = express.Router();
 
-mondayRouter.get("/", (req, res) => {
-    res.send("hello world");
-});
-
 mondayRouter.post("/", (req, res) => {
-    const challenge = req.body.challenge;
-    console.log(req);
+    const { challenge } = req.body;
+    console.log(req.body);
     res.status(200).send(JSON.stringify({challenge: challenge}));
   });
 module.exports = mondayRouter;
